b24be298a6a573509ce06dee6704b7097b5ec593
unknown <d0703887@gmail.com>
2025-05-09 20:27:01 -0700

    tmp

--- Kaggle-TrajectoryForecasting/train.py
+++ Kaggle-TrajectoryForecasting/train.py
@@ -243,7 +243,7 @@ if __name__ == "__main__":
 
     model = Decoder(config)
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    split_val = True
+    split_val = False
 
     if config.use_sampling:
         train_data = ArgoverseSocialAttnSampling('train', split_val, config.dataset_path)
