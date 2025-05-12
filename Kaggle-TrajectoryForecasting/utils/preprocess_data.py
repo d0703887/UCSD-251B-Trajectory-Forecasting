fe901251e175fa865c848771768dfa0fe68371d0
unknown <d0703887@gmail.com>
2025-05-12 00:36:16 -0700

    tmp

--- Kaggle-TrajectoryForecasting/utils/preprocess_data.py
+++ Kaggle-TrajectoryForecasting/utils/preprocess_data.py
@@ -8,8 +8,8 @@ class Argoverse(Dataset):
     def __init__(self, mode: str = 'train', split_val: bool = True, dataset_path: str = "dataset"):
         self.data = torch.tensor(load_dataset(dataset_path)[0], dtype=torch.float32)
         if split_val:
-            self.data = self.data[:int(len(self.data) * 0.7)] if mode == 'train' else self.data[
-                                                                                      int(len(self.data) * 0.7):]
+            self.data = self.data[:int(len(self.data) * 0.8)] if mode == 'train' else self.data[
+                                                                                      int(len(self.data) * 0.8):]
 
         if mode == 'train':
             self.data = self.data[self.data[:, :, 0, 5] == 0]
